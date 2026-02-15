a4308c8040af6c8fe5f5af65b7bb441580364266
Pranjal Singh Pokhariya <themonkemode@gmail.com>
2026-02-15 18:29:21 +0530

    Added animations to the how to use

--- script.js
+++ script.js
@@ -76,6 +76,8 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
 }, { threshold: 0.4 });
